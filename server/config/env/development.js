1b18532f7862e597780af5a551b3e2b20f61fea2
Ryan Connelly <ryan@atlantacs.com>
2014-07-08 11:45:21 -0400

    Added cadence package

--- server/config/env/development.js
+++ server/config/env/development.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-    db: 'mongodb://localhost/mean-dev',
+    db: 'mongodb://localhost/cadence',
     app: {
         name: 'MEAN - FullStack JS - Development'
     },
